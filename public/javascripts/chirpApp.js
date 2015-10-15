bfb50feec6aa7739399dc5602ca2b6605e395a9f
zenzontle <zenzontle@gmail.com>
2015-10-15 16:09:10 -0700

    Create templates and base page

--- public/javascripts/chirpApp.js
+++ public/javascripts/chirpApp.js
@@ -1,4 +1,23 @@
-var app = angular.module('chirpApp', []);
+var app = angular.module('chirpApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		//the timeline display
+		.when('/', {
+			templateUrl: 'main.html',
+			controller: 'mainController'
+		})
+		//the login display
+		.when('/login', {
+			templateUrl: 'login.html',
+			controller: 'authController'
+		})
+		//the signup display
+		.when('register', {
+			templateUrl: 'register.html',
+			controller: 'authController'
+		});
+});
 
 app.controller('mainController', function($scope) {
 	$scope.posts = [];
